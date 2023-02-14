3da9919c1b24a0162efe73d8b90231771e225a6a
imanmalhi <imanmalhi7@gmail.com>
2023-02-14 12:30:41 -0500

    commit

--- smartcontract/constants/index.js
+++ smartcontract/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x8e6b8C6149fcea3489758F05990873bC0DA1aFC5";
 // URL to extract Metadata for a NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-ui-steel.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
